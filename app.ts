ea1f8051ce2c6327701cba2c0bbf2e6eeed266cb
luowensheng <luowensheng2018@gmail.com>
2023-05-16 11:36:10 +0800

    test flow

--- app.ts
+++ app.ts
@@ -2,43 +2,43 @@ import type { ViewConstructor, View} from "./index"
 import {mountView} from "./index"
 
 
-const ButtonView: ViewConstructor = ({text="Button", click=()=>{ return console.log("button is pressed"); }}) => {
+// const ButtonView: ViewConstructor = ({text="Button", click=()=>{ return console.log("button is pressed"); }}) => {
 
-    return {
-            button: {
-                events: { click },
-                textContent: text,
-                style: { padding: "5%", "background-color": "blue", display: "flex", border: "2px solid red" }
-            }
-    }
-} 
+//     return {
+//             button: {
+//                 events: { click },
+//                 textContent: text,
+//                 style: { padding: "5%", "background-color": "blue", display: "flex", border: "2px solid red" }
+//             }
+//     }
+// } 
 
-const ContentView: View =  {
+// const ContentView: View =  {
         
-    div: {
-            HStack: [
-                {
-                    div: {
-                        VStack: [
-                            ButtonView({text: "Button1"}),          
-                            ButtonView({text: "Button2"}), 
-                        ]
-                    },
+//     div: {
+//             HStack: [
+//                 {
+//                     div: {
+//                         VStack: [
+//                             ButtonView({text: "Button1"}),          
+//                             ButtonView({text: "Button2"}), 
+//                         ]
+//                     },
                     
-                },
-                {
-                    div: {
-                        VStack: [
-                            ButtonView({text: "Button1"}),          
-                            ButtonView({text: "Button2"}), 
-                        ]
-                    },
+//                 },
+//                 {
+//                     div: {
+//                         VStack: [
+//                             ButtonView({text: "Button1"}),          
+//                             ButtonView({text: "Button2"}), 
+//                         ]
+//                     },
                     
-                }        
-            ]
-    },
+//                 }        
+//             ]
+//     },
 
-}
+// }
 
 
-mountView(ContentView, document.body)
+// mountView(ContentView, document.body)
